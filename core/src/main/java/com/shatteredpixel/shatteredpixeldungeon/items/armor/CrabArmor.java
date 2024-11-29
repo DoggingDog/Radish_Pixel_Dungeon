7df2c6e5d56ffdf4b31e3a895df8f95a91c47409
Prana Ling <70191651+LingASDJ@users.noreply.github.com>
2024-11-29 20:54:28 +0800

    修复部分楼层贴图异常

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/CrabArmor.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/CrabArmor.java
@@ -13,10 +13,12 @@ public class CrabArmor extends Armor{
     protected ArmorBuff buff( ) {
         return new CrabArmor.likeCrab();
     }
+
     @Override
     public int DRMax(int lvl){
         return 2 + Math.max( 2 * lvl + augment.defenseFactor(lvl), lvl);
     }
+
     @Override
     public int DRMin(int lvl){
         return 2 + lvl;
