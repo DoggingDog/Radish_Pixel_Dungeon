e1bb4f4988a3a4dd95bef7c0da403ec3228701e0
DoggingDog <chh1916308908@gmail.com>
2025-02-07 23:55:58 +0800

    ## fix on 20250314

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/FloatingText.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/effects/FloatingText.java
@@ -79,6 +79,8 @@ public class FloatingText extends RenderedTextBlock {
 
 	public static int CRIT    = 25;
 
+	public static int CRIT_NO_BLOCK = 26;
+
 	public static int EX_EXP    = 27;
 	public static int BELIEF    = 28;
 
